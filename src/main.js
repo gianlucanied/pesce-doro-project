efdb50a4c58f7ce20f382927d29e086c1919ae67
Gianluca Nieddu <gianluca.nieddu96@gmail.com>
2025-11-18 12:32:19 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
-
-import { createI18n } from 'vue-i18n'
 import translations from './translations.json'
 
 const i18n = createI18n({
@@ -20,4 +13,9 @@ const i18n = createI18n({
   messages: translations,
 })
 
+const app = createApp(App)
+
+app.use(router)
 app.use(i18n)
+
+app.mount('#app')
